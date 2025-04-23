27b48c88c35f9a7d0fa75814490bda66a7920446
Pranshu <sharmapranshu600@gmail.com>
2025-04-23 21:00:07 +0100

    Content added

---
+++ MyWebsite.js
@@ -0,0 +1,14 @@
+function Hscroll(){
+    document.getElementById("scroll").scrollBy({ left: -100, behavior: 'smooth' });
+}
+
+function scrollRight() {
+    document.getElementById("scroll").scrollBy({ left: 100, behavior: 'smooth' });
+}
+function Hscroll2(){
+    document.getElementById("scroll2").scrollBy({ left: -100, behavior: 'smooth' });
+}
+
+function scrollRight2() {
+    document.getElementById("scroll2").scrollBy({ left: 100, behavior: 'smooth' });
+}
